5cfa684c24e9c7dfa905dce2cd1ff67cca8d9692
Wahyu Agung Tribawono <wahyuagung26@gmail.com>
2017-09-30 08:54:17 +0700

    -

--- api/index.php
+++ api/index.php
@@ -4,6 +4,18 @@ use \Psr\Http\Message\ServerRequestInterface;
 
 session_start();
 
+# Session lifetime of 3 hours
+ini_set('session.gc_maxlifetime', 20 * 60 * 60);
+# Enable session garbage collection with a 1% chance of
+# running on each session_start()
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 100);
+# Our own session save path; it must be outside the
+# default system save path so Debian's cron job doesn't
+# try to clean it up. The web server daemon must have
+# read/write permissions to this directory.
+session_save_path(__DIR__ . '/api/sessions');
+
 require 'vendor/autoload.php';
 
 /** load .env file */
--- js/config.router.js
+++ js/config.router.js
@@ -23,7 +23,35 @@ angular.module('app').run(
                 });
             });
         }
-    ]).config(
+    ]);
+angular.module('app').config(function($httpProvider) {
+    $httpProvider.interceptors.push(function($q, $rootScope) {
+        var numberOfHttpRequests = 0;
+        return {
+            request: function(config) {
+                numberOfHttpRequests += 1;
+                $rootScope.waitingForHttp = true;
+                return config;
+            },
+            requestError: function(error) {
+                numberOfHttpRequests -= 1;
+                $rootScope.waitingForHttp = (numberOfHttpRequests !== 0);
+                return $q.reject(error);
+            },
+            response: function(response) {
+                numberOfHttpRequests -= 1;
+                $rootScope.waitingForHttp = (numberOfHttpRequests !== 0);
+                return response;
+            },
+            responseError: function(error) {
+                numberOfHttpRequests -= 1;
+                $rootScope.waitingForHttp = (numberOfHttpRequests !== 0);
+                return $q.reject(error);
+            }
+        };
+    });
+});
+angular.module('app').config(
     ['$stateProvider', '$urlRouterProvider',
         function($stateProvider, $urlRouterProvider) {
             $urlRouterProvider.otherwise('/site/dashboard');
