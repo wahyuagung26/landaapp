7528f58e4d1afaf5556ab3824feca1d5143d2eab
Wahyu Agung Tribawono <wahyuagung26@gmail.com>
2018-01-19 10:28:36 +0700

    -

--- api/index.php
+++ api/index.php
@@ -4,26 +4,33 @@
 ini_set('session.gc_maxlifetime', 20 * 60 * 60);
 ini_set('session.gc_probability', 1);
 ini_set('session.gc_divisor', 100);
-session_save_path(__DIR__ . '/api/sessions');
+if (!file_exists(__DIR__ . '/sessions')) {
+    mkdir(__DIR__ . '/sessions', 0777, true);
+}
+session_save_path(__DIR__ . '/sessions');
 
 session_start();
 
 require 'vendor/autoload.php';
 
-/** load .env file */
-$dotenv = new Dotenv\Dotenv(__DIR__);
-
 $subDomain = explode('.', $_SERVER['HTTP_HOST']);
 if (isset($subDomain[0]) && $subDomain[0] == 'tes') {
+    /** load .env file */
+    $dotenv = new Dotenv\Dotenv(__DIR__ . '/config/', "test.env");
     $dotenv->load();
 } else {
-    $dotenv->load('tes.env');
+    /** load .env file */
+    $dotenv = new Dotenv\Dotenv(__DIR__ . '/config/', ".env");
+    $dotenv->load();
 }
 
 /* --- System --- */
 require 'systems/database.php';
 require 'systems/systems.php';
 require 'systems/functions.php';
+if (file_exists('vendor/cahkampung/landa-php/src/LandaPhp.php')) {
+    require 'vendor/cahkampung/landa-php/src/LandaPhp.php';
+}
 
 $display = (getenv('DISPLAY_ERRORS') == 'true') ? true : false;
 
