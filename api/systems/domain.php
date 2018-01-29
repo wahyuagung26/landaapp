9aca9081ad2a192ddb219fe2eab8609a790b3a03
Wahyu Agung Tribawono <wahyuagung26@gmail.com>
2018-01-29 14:49:55 +0700

    -

--- api/systems/domain.php
+++ api/systems/domain.php
@@ -2,10 +2,10 @@
 $subDomain = explode('.', $_SERVER['HTTP_HOST']);
 if (isset($subDomain[0]) && $subDomain[0] == 'tes') {
     /** load .env file */
-    $dotenv = new Dotenv\Dotenv(__DIR__ . '/config/', "test.env");
+    $dotenv = new Dotenv\Dotenv('config/', "test.env");
     $dotenv->load();
 } else {
     /** load .env file */
-    $dotenv = new Dotenv\Dotenv(__DIR__ . '/config/', ".env");
+    $dotenv = new Dotenv\Dotenv('config/', ".env");
     $dotenv->load();
 }
