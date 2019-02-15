089a904a85d7b64928fadd081fec852181a6f486
Wahyu Agung <wahyuagung26@gmail.com>
2019-02-15 14:41:38 +0700

    -

--- js/lazyload.js
+++ js/lazyload.js
@@ -4,36 +4,36 @@ angular.module('app').config(['$ocLazyLoadProvider', function($ocLazyLoadProvide
         events: true,
         modules: [{
             name: 'daterangepicker',
-            files: ['vendor/bootstrap-daterangepicker/daterangepicker.js', 'vendor/angular-daterangepicker/js/angular-daterangepicker.min.js', 'vendor/bootstrap-daterangepicker/daterangepicker.css']
+            files: ['library/bootstrap-daterangepicker/daterangepicker.js', 'library/angular-daterangepicker/js/angular-daterangepicker.min.js', 'library/bootstrap-daterangepicker/daterangepicker.css']
         }, {
             name: 'angularFileUpload',
-            files: ['vendor/angular-file-upload/dist/angular-file-upload.min.js']
+            files: ['library/angular-file-upload/dist/angular-file-upload.min.js']
         }, {
             name: 'ngFileUpload',
-            files: ['vendor/ngfileupload/ng-file-upload-shim.js', 'vendor/ngfileupload/ng-file-upload.js']
+            files: ['library/ngfileupload/ng-file-upload-shim.js', 'library/ngfileupload/ng-file-upload.js']
         }, {
             serie: true,
             name: 'naif.base64',
-            files: ['vendor/angular-base64-upload/dist/angular-base64-upload.min.js']
+            files: ['library/angular-base64-upload/dist/angular-base64-upload.min.js']
         }, {
             name: 'frAngular',
-            files: ['vendor/keyboard-manager/keyboardManager.js']
+            files: ['library/keyboard-manager/keyboardManager.js']
         }, {
             name: 'mgo-angular-wizard',
-            files: ["vendor/angular-wizard/dist/angular-wizard.min.js", "vendor/angular-wizard/dist/angular-wizard.min.css"]
+            files: ["library/angular-wizard/dist/angular-wizard.min.js", "library/angular-wizard/dist/angular-wizard.min.css"]
         }, {
             name: 'fontawesome',
-            files: ["vendor/font-awesome/css/font-awesome.min.css"]
+            files: ["library/font-awesome/css/font-awesome.min.css"]
         }, {
             name: 'simplelineicon',
-            files: ["vendor/simple-line-icons/css/simple-line-icons.css"]
+            files: ["library/simple-line-icons/css/simple-line-icons.css"]
         }, {
             name: 'iconflag',
-            files: ["vendor/flag-icon-css/css/flag-icon.min.css"]
+            files: ["library/flag-icon-css/css/flag-icon.min.css"]
         }, {
             serie: true,
             name: 'chart.js',
-            files: ['vendor/chart.js/dist/Chart.min.js', 'vendor/angular-chart.js/dist/angular-chart.min.js']
+            files: ['library/chart.js/dist/Chart.min.js', 'library/angular-chart.js/dist/angular-chart.min.js']
         }]
     });
 }]);
