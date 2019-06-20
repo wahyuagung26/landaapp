f098a63d0e9b5cea0b25e48d569e7ca0a2cb6752
Wahyu Agung <wahyuagung26@gmail.com>
2019-06-20 13:26:24 +0700

    toaster

--- tpl/site/login.js
+++ tpl/site/login.js
@@ -1,4 +1,4 @@
-app.controller("loginCtrl", function($scope, Data, toaster, $state, $rootScope, UserService, $timeout, $location) {
+app.controller("loginCtrl", function($scope, Data, $state, $rootScope, UserService, $timeout, $location) {
     $scope.authError = null;
     $scope.form = {};
     var user = UserService.getUser();
@@ -10,7 +10,7 @@ app.controller("loginCtrl", function($scope, Data, toaster, $state, $rootScope, 
         $scope.authError = null;
         Data.post("site/login", form).then(function(result) {
             if (result.status_code != 200) {
-                toaster.pop("error", "Terjadi Kesalahan", setErrorMessage(result.errors));
+                $rootScope.alert("Terjadi Kesalahan", setErrorMessage(result.errors), "error");
             } else {
                 UserService.setUser(result.data.user);
                 $rootScope.user = UserService.getUser();
